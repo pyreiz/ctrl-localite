470d88f77e67fd0c7d3ebcdae2f3469d92351d5c
agricolab <robertsadresse@gmx.de>
2019-05-17 08:55:52 +0200

    Add Markerstreamer

--- localite/__init__.py
+++ localite/__init__.py
@@ -13,103 +13,100 @@ class Coil():
     def __init__(self, coil=0, **kwargs):
         self.id = coil
         self._client = Client(**kwargs)
-        self._send = self._client.send
-        self._request = self._client.request
+       
+    def activate(self):
+        self._send('{"current_instrument":"coil_' + self.id + '}')        
+        return self._request('{"get":"current_instrument"}')[1]
+
+    def request(self, msg:str):
+        return self._client.request("coil_" + self.id + "_" + msg)
     
+    def send(self, key:str, val:str):
+        self._client.send('{"coil_' + self.id + '_' + key + '": ' + val + '}')        
     
-    def _get_id(self):
+    @property
+    def id(self):
         return str(self._id)
     
-    def _set_id(self, coil:int=0):
+    @id.setter
+    def id(self, coil:int=0):
         if coil not in (0, 1):
             raise ValueError("Coil must be 0  or 1")
         self._id= coil
     
-    id = property(_get_id, _set_id)
-    
-          
     @property
     def type(self):
-        return self._request('{"get":"coil_'+ self.id +'_type"}')[1]
+        return self.request("type")
     
     @property
     def temperature(self):
-        return self._request('{"get":"coil_'+ self.id +'_temperature"}')[1]
+        return self.request("temperature")
     
     @property
     def waveform(self):
-        return self._request('{"get":"coil_'+ self.id +'_waveform"}')[1]
+        return self.request("waveform")
     
     @property
     def stimulator_mode(self):
-        return self._request('{"get":"coil_'+ self.id +'_stimulator_mode"}')[1]
+        return self.request("stimulator_mode")
     
     @property
     def didt(self):
-        return self._request('{"get":"coil_'+ self.id +'_didt"}')[1]
+        return self.request("didt")
  
-
-    def _get_amplitude(self):
-        return self._request('{"get":"coil_'+ self.id +'_amplitude"}')[1]
+    @property
+    def amplitude(self):
+        return self.request("amplitude")
     
-    def _set_amplitude(self, amplitude:int):
-        self._send('{"coil_' + self.id + '_amplitude": ' + str(amplitude) + '}')        
-        return self._request('{"get":"coil_'+ self.id +'_amplitude"}')[1]
-
-    amplitude = property(_get_amplitude, _set_amplitude)
+    @amplitude.setter
+    def amplitude(self, amplitude:int):
+        self.send("amplitude", str(amplitude))
+        return self.request("amplitude")
 
     @property
-    def _get_target_index(self):
-        return self._request('{"get":"coil_' + self.id + '_target_index"}')[1]
+    def target_index(self):
+        return self.request("target_index")
   
-    
-# %% 
-class Localite(Client):
-    
-    def __init__(self, coil=0, **kwargs):
-        self.coil = coil
-        super().__init__(**kwargs)
-    
-    
-    def get_coil(self):
-        return str(self._coil)
-    
-    def set_coil(self, coil:int=0):
-        if coil not in (0, 1):
-            raise ValueError("Coil must be 0  or 1")
-        self._coil = coil
-    
-    coil = property(get_coil, set_coil)
-    
+    @target_index.setter
+    def target_index(self, index:int):
+        self.send("target_index", str(index))
+        return self.request("target_index")
     
     @property
-    def coil_target_index(self):
-        return self.request('{"get":"coil_' + self.coil + '_target_index"}')
+    def response(self):
+        return self.request("response")
+    
+    @property
+    def position(self):
+        return self._request("response")
+    
+    def trigger(self):
+        self._client.send('{"single_pulse":"COIL_' + str(self.id) + '"}')
+    
+    
+# %%
+# c.amplitude = 1        
+# %timeit -n 1 -r 1000 c.trigger()
+# %% 
+class Localite():
+    
+    def __init__(self, coil:int=0, host:str="127.0.0.1", port:int=6666):
+        self._client = Client(host=host, port=port)
+        self.coil = coil              
         
     @property
-    def coil_type(self):
-        return self.request('{"get":"coil_'+ self.coil +'_type"}')
+    def coil(self):
+        return self._coil
     
+    @coil.setter
+    def coil(self, coil:int=0):
+        if coil not in (0, 1):
+            raise ValueError("Coil must be 0  or 1")
+        self._coil = Coil(coil=coil, host=self._client.host, 
+                          port=self._client.port)
+        
     @property
-    def coil_temperature(self):
-        return self.request('{"get":"coil_'+ self.coil +'_temperature"}')
-    
-    @property
-    def coil_waveform(self):
-        return self.request('{"get":"coil_'+ self.coil +'_waveform"}')
-    
-    @property
-    def coil_stimulator_mode(self):
-        return self.request('{"get":"coil_'+ self.coil +'_stimulator_mode"}')
-    
-    @property
-    def coil_didt(self):
-        return self.request('{"get":"coil_'+ self.coil +'_didt"}')
- 
-    @property
-    def coil_amplitude(self):
-        return self.request('{"get":"coil_'+ self.coil +'_amplitude"}')
-    
-    
+    def navigation_mode(self):
+        return self._client.request("navigation_mode")
     
 # %%
--- localite/client.py
+++ localite/client.py
@@ -6,9 +6,43 @@ A client to communicate with the Localite to control the magventure.
 """
 import socket
 import json
+import pylsl
+import threading
 # %%
+    
+class MarkerStreamer(threading.Thread):    
+    "LSL based software marker streamer"
 
-
+    def __init__(self, name:str="localite_marker", host="127.0.0.1", port=6666): 
+        threading.Thread.__init__(self)           
+        self.host = host
+        self.port = port        
+        self.name = name
+        self.is_running = threading.Event()
+        
+    def stop(self):
+        self.queue.join()
+        self.is_running.clear()
+                
+    def run(self):   
+        source_id = socket.gethostname()
+        info = pylsl.StreamInfo(self.name, type='Markers', channel_count=1, nominal_srate=0, 
+                                channel_format='string', source_id=source_id)        
+        outlet = pylsl.StreamOutlet(info)
+        client = Client(host=self.host, port=self.port)
+        client.timeout = None # no timeout
+                
+        self.is_running.set()
+        print(info.as_xml())        
+        while self.is_running.is_set(): 
+            key, val = client.listen()
+            if key in ('coil_0_didt', 'coil_1_didt'): #localite has triggered
+                marker = json.dumps({key:val})
+                tstamp = pylsl.local_clock()
+                outlet.push_sample([marker])   
+                print(f'Pushed {marker} at {tstamp}')
+                
+# %%            
 class Client(object):
     """
      A LocaliteJSON socket client used to communicate with a LocaliteJSON socket server.
@@ -29,15 +63,16 @@ class Client(object):
 
     socket = None
 
-    def __init__(self, host, port=6666):        
+    def __init__(self, host, port=6666, timeout=3):        
         self.host = host
         self.port = port
+        self.timeout= timeout
                 
     def connect(self):
         'connect wth the remote server'
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.host, self.port))
-        self.socket.settimeout(3)
+        self.socket.settimeout(self.timeout)
 
     def close(self):
         'closes the connection'
@@ -63,6 +98,12 @@ class Client(object):
             except Exception as e:
                 raise e       
     
+    def listen(self):
+        self.connect()
+        msg = self.read()
+        self.close()
+        return msg
+    
     def decode(self, msg:str, index=0):
         msg = json.loads(msg)
         key = list(msg.keys())[index]
@@ -74,19 +115,17 @@ class Client(object):
         self.write(msg)
         self.close()
         
-    def request(self, msg='{"get":"coil_0_amplitude"}'):
+    def request(self, msg="coil_0_amplitude"):
         self.connect()
+        msg = '{"get":\"' + msg + '\"}'
         self.write(msg)        
         key = val = ''    
         _, expected = self.decode(msg)   
         while key != expected:
-            key, val = self.read() 
-        self.close()
-        return key, val
+            key, val = self.read()         
+        self.close()        
+        return None if val == 'NONE' else val
 
-# c = Client(host=host))
+# c = Client(host=host)
 # %timeit -n 1 -r 1000 c.request()        
-# 5.38 ms ± 244 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
-# %%
-# %timeit -n 1 -r 1000 c.send('{"coil_0_amplitude":45}')        
-# 451 µs ± 12.6 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
+# 5.22 ms ± 3.29 ms per loop (mean ± std. dev. of 1000 runs, 1 loop each)
