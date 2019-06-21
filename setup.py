9d5ffaf40d5151645711fd8d566741e1b920f733
agricolab <robertsadresse@gmx.de>
2019-06-21 14:04:34 +0200

    Pump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='ctrl-localite',
-    version='0.0.1',
+    version='0.0.2',
     description='Control Magventure with LocaliteJSON',
     long_description='Toolbox to control a Magventure TMS with localites JSON-TCP-IP Interface',
     author='Robert Guggenberger',
