ec2c5447bfcc845635f41cb38d2b120e0f55ce64
agricolab <robertsadresse@gmx.de>
2019-10-28 19:08:05 +0100

    Start refactoring into ReceiverServer and Client

--- localite/client.py
+++ localite/client.py
@@ -4,57 +4,142 @@ A client to communicate with the Localite to control the magventure.
 
 @author: Robert Guggenberger
 """
+# %%
 import socket
 import json
 import pylsl
 import threading
 import time
-import logging
-logger = logging.getLogger("localite.client")
-logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler(r"C:\Users\AGNPT-M-001\Desktop\localite.log")
-fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-logger.addHandler(ch)
-logger.debug('initialize logger to '+ r"C:\Users\AGNPT-M-001\Desktop\localite.log")
+from typing import Callable
+
+def is_port_in_use(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex((host, port)) == 0
+
+
+def get_client(host='127.0.0.1', port=6666):
+    if is_port_in_use("127.0.0.1", port+1):
+        kill_client()
+    client = SmartClient(host=host, port=port)
+    return client.start()
+
+def kill_client(host='127.0.0.1', port=6667):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        msg = json.dumps({"command":"die"})
+        s.send(msg)
+
+
+
 # %%
-class SmartClient(threading.Thread):    
-    "LSL based software marker streamer"
-
-    instance = [None]
-    @classmethod
-    def get_running_instance(cls, **kwargs):
-        if cls.instance[0] is None or not cls.instance[0].is_alive:
-            cls.instance[0] = cls(**kwargs)
-        if not cls.instance[0].is_running.is_set():
-            cls.instance[0].start()            
-        return cls.instance[0]            
-
-    def __init__(self, name:str="localite_marker", host="127.0.0.1", port=6666): 
-        threading.Thread.__init__(self)           
+class SmartClient():
+    def __init__(self, name:str="localite_marker", host:str="127.0.0.1", port:int=6666):
+        self.name = name
+        self.port = port
         self.host = host
-        self.port = port        
-        self.name = name    
-        
-        self.client_lock = threading.Lock()
-        self.client = Client(host=self.host, port=self.port)
-   
-        source_id = '_'.join((socket.gethostname(), name))
-        self.info = pylsl.StreamInfo(self.name, type='Markers', channel_count=1, nominal_srate=0, 
-                                channel_format='string', source_id=source_id)        
-        self.outlet = pylsl.StreamOutlet(self.info)     
-        self.outlet_lock = threading.Lock()
 
-        self.is_running = threading.Event()        
-        
+    def stop(self):
+        kill_client()
+
+    def start(self):
+        port = self.port
+        host = self.host
+        name = self.name
+        client = ReceiverClient(name=name, host=host, port=port)
+        killer = ReceiverServer(host="127.0.0.1", port=port+1, foo=client.stop)
+        client.start()
+        killer.start()
+        return client
+    
+
+# %%
+
+
+class ReceiverServer(threading.Thread):
+
+    def __init__(self, host="127.0.0.1", port: int = 6667, foo:Callable=None):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.port = port
+        self.stop_client = foo
+        self.is_running = threading.Event()
+
     def stop(self):
         self.is_running.clear()
-             
-    def send(self, msg:str):
+        print("Shutting down RecevierServer at", self.host, self.port)
+
+    def run(self):
+        interface = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        interface.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        interface.settimeout(1)
+        interface.bind((self.host, self.port))
+        interface.listen(1)
+        print('Server distributing Coil Clients opened at {0}:{1}'.format(
+            self.host, self.port))
+        self.is_running.set()
+        while self.is_running.is_set():
+            message = {"command":"live"}
+            try:
+                client, address = interface.accept()
+                try:
+                    message = read_msg(client)
+                except socket.timeout:
+                    print('Client from {address} timed out')
+                finally:
+                    client.shutdown(2)
+                    client.close()
+            except socket.timeout:
+                pass
+            if message["command"] == "die":
+                self.stop_client()
+                self.stop()
+
+
+# %%
+
+
+class ReceiverClient(threading.Thread):
+    "LSL based software marker streamer"
+
+    def __init__(self, name:str="localite_marker", host:str="127.0.0.1", port=6666):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.port = port
+        self.name = name
+
+        self.client_lock = threading.Lock()
+        self.client = Client(host=self.host, port=self.port)
+
+        source_id = '_'.join((socket.gethostname(), name))
+        self.info = pylsl.StreamInfo(self.name, type='Markers', channel_count=1,
+                                     nominal_srate=0, channel_format='string', source_id=source_id)
+        self.outlet = pylsl.StreamOutlet(self.info)
+        self.outlet_lock = threading.Lock()
+        self.is_running = threading.Event()
+
+    def run(self):
+        self.is_running.set()
+        print(self.info.as_xml())
+        while self.is_running.is_set():
+            try:
+                with self.client_lock:
+                    key, val = self.client.listen()
+            except (ConnectionResetError, ConnectionRefusedError):
+                print("Connection Problems. Retrying")
+
+            tstamp = pylsl.local_clock()
+            marker = json.dumps({key: val})
+
+            if key in ('coil_0_didt', 'coil_1_didt'):  # localite has triggered
+                print(f'Pushed {marker} at {tstamp}')
+                with self.outlet_lock:
+                    self.outlet.push_sample([marker], tstamp)
+
+    def stop(self):
+        self.is_running.clear()
+        print("Shutting down ReceiverClient for", self.host, self.port)
+
+    def send(self, msg: str):
         with self.client_lock:
             try:
                 self.client.send(msg)
@@ -63,62 +148,47 @@ class SmartClient(threading.Thread):
                 print("Connection Problems. I'll keep on trying to connect")
                 time.sleep(5)
                 self.client = Client(host=self.host, port=self.port)
-             
-                    
+
     def request(self, msg):
         try:
             with self.client_lock:
-                answer = self.client.request(msg)                        
+                answer = self.client.request(msg)
         except (ConnectionResetError, ConnectionRefusedError):
             print("Connection Problems. Retrying")
-    
-        print(f'Received {answer} for {msg} at {pylsl.local_clock()}')                
-        return answer 
-    
-    def trigger(self, id:str):        
-        marker = '{"single_pulse":"COIL_' + id + '"}'                
-        try:                                
+
+        print(f'Received {answer} for {msg} at {pylsl.local_clock()}')
+        return answer
+
+    def trigger(self, id: str):
+        marker = '{"single_pulse":"COIL_' + id + '"}'
+        try:
             with self.client_lock:
-               self.client.send(marker)                                
+                self.client.send(marker)
         except (ConnectionResetError, ConnectionRefusedError):
             print("Connection Problems. Aborting for safety")
             return None
-                
+
         tstamp = pylsl.local_clock()
         print(f'Pushed {marker} at {tstamp}')
-        with self.outlet_lock:            
-            self.outlet.push_sample([marker], tstamp)                   
-        
-        return tstamp
-    
-    def push_marker(self, marker:str):
-        tstamp = pylsl.local_clock()
-        print(f'Pushed {marker} at {tstamp}')
-        with self.outlet_lock:            
+        with self.outlet_lock:
             self.outlet.push_sample([marker], tstamp)
+
+        return tstamp
+
+    def push_marker(self, marker:str):
+
+        tstamp = pylsl.local_clock()
+
+        print(f'Pushed {marker} at {tstamp}')
+
+        with self.outlet_lock:            
+
+            self.outlet.push_sample([marker], tstamp)
+
                     
-    def run(self):   
-        self.is_running.set()
-        print(self.info.as_xml())        
-        while self.is_running.is_set():                
-            try:
-                with self.client_lock:    
-                    key, val = self.client.listen()       
-            except (ConnectionResetError, ConnectionRefusedError):
-                print("Connection Problems. Retrying")
-            
-            
-            tstamp = pylsl.local_clock()            
-            marker = json.dumps({key:val})   
-            
-            if key in ('coil_0_didt', 'coil_1_didt'): #localite has triggered                
-                print(f'Pushed {marker} at {tstamp}')
-                with self.outlet_lock:
-                    self.outlet.push_sample([marker], tstamp)                   
-   
-    
-                
- # %%            
+# %%
+
+
 class Client(object):
     """
      A LocaliteJSON socket client used to communicate with a LocaliteJSON socket server.
@@ -139,11 +209,11 @@ class Client(object):
 
     socket = None
 
-    def __init__(self, host, port=6666, timeout=None):        
+    def __init__(self, host, port=6666, timeout=None):
         self.host = host
         self.port = port
-        self.timeout= timeout
-                
+        self.timeout = timeout
+
     def connect(self):
         'connect wth the remote server'
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -155,21 +225,19 @@ class Client(object):
         self.socket.shutdown(1)
         self.socket.close()
         del self.socket
-        
+
     def write(self, data):
         self.socket.sendall(data.encode('ascii'))
         return self
 
-
     def read_byte(self, counter, buffer):
         """read next byte from the TCP/IP bytestream and decode as ASCII"""
         if counter is None:
-             counter = 0
+            counter = 0
         char = self.socket.recv(1).decode('ASCII')
         buffer.append(char)
         counter += {'{': 1, '}': -1}.get(char, 0)
         return counter, buffer
-
 
     def read(self):
         'parse the message until it is a valid json'
@@ -185,37 +253,43 @@ class Client(object):
         msg = self.read()
         self.close()
         return msg
-    
-    def decode(self, msg:str, index=0):
-        msg = msg.replace('reason','\"reason\"') #catches and interface error    
+
+    def decode(self, msg: str, index=0):
+        # catches and interface error
+        msg = msg.replace('reason', '\"reason\"')
         try:
             decoded = json.loads(msg)
         except json.JSONDecodeError as e:
-            logger.error("JSONDecodeError: " +  msg)
+            logger.error("JSONDecodeError: " + msg)
             raise e
-                
+
         key = list(decoded.keys())[index]
         val = decoded[key]
         return key, val
-    
-    def send(self, msg:str):
+
+    def send(self, msg: str):
         self.connect()
         self.write(msg)
         self.close()
-        
+
     def request(self, msg="coil_0_amplitude"):
         self.connect()
         msg = '{"get":\"' + msg + '\"}'
-        self.write(msg)        
-        key = val = ''    
-        _, expected = self.decode(msg)   
+        self.write(msg)
+        key = val = ''
+        _, expected = self.decode(msg)
         logger.debug(msg)
         logger.debug(expected)
         while key != expected:
-            key, val = self.read()         
-        self.close()        
+            key, val = self.read()
+        self.close()
         return None if val == 'NONE' else val
 
+
 # c = Client(host=host)
-# %timeit -n 1 -r 1000 c.request()        
+# %timeit -n 1 -r 1000 c.request()
 # 5.22 ms ± 3.29 ms per loop (mean ± std. dev. of 1000 runs, 1 loop each)
+if __name__ == "__main__":
+
+    client = get_client()
+  
--- localite/coil.py
+++ localite/coil.py
@@ -5,9 +5,11 @@
 @author: Robert Guggenberger
 """
 
-from localite.client import SmartClient
+from localite.client import get_client
 from json import dumps as _dumps
 # %%
+
+
 class Coil():
     """Coil is a user-friendly interface to control the TMS and Localite
 
@@ -18,107 +20,108 @@ class Coil():
 
     it wraps a SmartClient, and processes Responses
     """
-    def __init__(self, coil:int=0, **kwargs):
-        self.id = coil
-        self._client = SmartClient.get_running_instance(**kwargs)
 
-    def push_dictionary(self, marker:dict):
+    def __init__(self, coil: int = 0, **kwargs):
+        self.id = coil
+        self._client = get_client(**kwargs)
+
+    def push_dictionary(self, marker: dict):
         "json encodes a dictionary before pushing it with `~.push_marker`"
-        self._client.push_marker(_dumps(marker))   
-        
-    def push_marker(self, marker:str):
+        self._client.push_marker(_dumps(marker))
+
+    def push_marker(self, marker: str):
         "pushes a str to the Marker-Stream running in the background"
         self._client.push_marker(marker)
-        
-    def request(self, msg:str):
+
+    def request(self, msg: str):
         return self._client.request("coil_" + self.id + "_" + msg)
-    
-    def send(self, key:str, val:str):
-        self._client.send('{"coil_' + self.id + '_' + key + '": ' + val + '}')        
-    
+
+    def send(self, key: str, val: str):
+        self._client.send('{"coil_' + self.id + '_' + key + '": ' + val + '}')
+
     def activate(self):
-        self._client.send('{"current_instrument":"COIL_' + self.id +'"}')
-    
+        self._client.send('{"current_instrument":"COIL_' + self.id + '"}')
+
     def trigger(self):
         """trigger a tms pulse for the currently selected coil
-        
+
         returns
         -------
         tstamp:float
             the pylsl timestamp when the trigger command was sent via TCP-IP
         """
-        return self._client.trigger(self.id) 
-    
+        return self._client.trigger(self.id)
+
     @property
     def id(self):
         """The coils id {0,1}
-        
+
         localite can control 2 coils, this parameter identifies which one is
         controlled by this instance. Indexing starts at 0.
         """
         return str(self._id)
-    
+
     @id.setter
-    def id(self, coil:int=0):
+    def id(self, coil: int = 0):
         if coil not in (0, 1):
             raise ValueError("Coil must be 0  or 1")
-        self._id= coil
-    
+        self._id = coil
+
     @property
     def type(self):
         return self.request("type")
-    
+
     @property
     def temperature(self):
         return self.request("temperature")
-    
+
     @property
     def waveform(self):
         return self.request("waveform")
-    
+
     @property
     def stimulator_mode(self):
         return self.request("stimulator_mode")
-    
+
     @property
     def didt(self):
         return self.request("didt")
- 
+
     @property
     def amplitude(self):
         return self.request("amplitude")
-    
+
     @amplitude.setter
-    def amplitude(self, amplitude:int):
+    def amplitude(self, amplitude: int):
         self.send("amplitude", str(amplitude))
         return self.request("amplitude")
 
     @property
     def target_index(self):
         return self.request("target_index")
-  
+
     @target_index.setter
-    def target_index(self, index:int):
+    def target_index(self, index: int):
         self.send("target_index", str(index))
         return self.request("target_index")
 
     @property
     def position(self):
         return self.request("position")
-        
+
     @property
     def position_reached(self):
         return True if self.request("position_control")['position_reached'] == "TRUE" else False
-    
+
     @property
     def status(self):
         return self.request("status")
-    
+
     @property
     def response(self):
         return self.request("response")
-    
-    def set_response(self, response, channel_idx:int=0):
+
+    def set_response(self, response, channel_idx: int = 0):
         """send the response in localite
 
         THe TMS-Navigator software must be set to receive them. Do so by 
